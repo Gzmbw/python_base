b6537dccf7e1dc549a4403ffb343dc5fc73023f4
GZMbw <unbroken1127@gmail.com>
2017-08-21 22:33:24 +0800

    lianxi

--- xiti/python_xiti/temp.py
+++ xiti/python_xiti/temp.py
@@ -1,28 +1,8 @@
- #coding: utf-8 
+name = 'jianguo'
+print name
+a=23
+b=33
+print a+b
 
-"""
-Spyder Editor
-
-This is a temporary script file.
-"""
-'''
-from random import randint
-num = randint(1,100)
-bingo=False
-while bingo!=True:
-    i=input()
-    if i<num:
-        print '%d is too small.' %i
-    if i>num:
-        print '%d is too big.' %i
-    if i==num:
-        print '%d is the right answer!' %i
-        bingo=True
-
-for i in range(0,5):
-    print '* * * * *'
-'''
-for i in range(0,5):
-    for j in range(0,i+1):
-        print '*',
-    print
+print 'hello pycharm'
+print 'sdjfh'
--- xiti/python_xiti/xiti1.py
+++ xiti/python_xiti/xiti1.py
@@ -5,4 +5,60 @@ Created on Mon Aug 21 16:54:29 2017
 
 @author: guo
 """
-print 'jianguo'
+
+'''
+for i in range(0,5):
+    for j in range(0,i+1):
+        print'*',
+    print
+
+print '%s is easy to learn ' %'python'
+
+print '''
+#%s's score is %d
+''' % ('mike',87)
+name='jianguo'
+score=87
+print "%s's score is %d" % (name,score)
+
+
+a=1
+print a
+a='hello'
+print a
+a=True 
+print a
+
+a=12
+b=str(a)
+print b
+type(b)
+
+
+
+print bool('False')
+
+
+a="123"
+if a:
+    print "this is not a blank string"
+
+for i in range(1,11):
+    for j in range(1,i):
+        print i,j
+
+name=input()
+print name
+name=raw_input()
+print name
+'''
+def sayhello():
+    print 'hello world!'
+sayhello()
+
+def sayhello(someone):
+    print someone + 'sayhello'
+sayhello(input())
+def fun(a,b):
+    print a+b
+fun(1,2)
